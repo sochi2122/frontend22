dd47e5d9c3667ce014a687f9ee505c548fa7c734
sochi2122 <90932673+sochi2122@users.noreply.github.com>
2022-01-11 20:22:56 -0500

    URL changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,9 @@ import Show from "../pages/Show"
 function Main(props) {
   const [people, setPeople] = useState(null)
 
-  const URL = "http://localhost:3001/people/"
+  //const URL = "http://localhost:3001/people/"
+
+  const URL = "https://zen-sinoussi-eb20dd.netlify.app/people"
 
   const getPeople = async () => {
     const response = await fetch(URL)
